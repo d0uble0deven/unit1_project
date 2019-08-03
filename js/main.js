4374354ec1df6bb32fd2527103813fc568f29a09
Dev Govindji <friendsofdev94@gmail.com>
2019-08-02 19:36:24 -0500

    complete game logic

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 var problems = [
     {
     question: "5+5=?\n(a) 3\n(b) 10\n(c) 16",
-    answer: "10"
+    answer: "b"
     },
     {
     question: "2+2=?\n(a) 8\n(b) 4\n(c) 1",
-    answer: "4"
+    answer: "b"
     }
 ]
 var score = 0;
 
 for(var i=0; i < problems.length; i++){
-    var ask = window.prompt(problems[i].prompt);
     //iterate through questions and alert user
-    alert(ask == problems[i].question);
+    alert(problems[i].question);
+    var ask = window.prompt(problems[i].prompt); 
     //checks if answer is correct and alerts user
     if(ask == problems[i].answer){
         score++;
@@ -22,6 +22,8 @@ for(var i=0; i < problems.length; i++){
         alert("You didn't get it :(");
     }
 }
+
+// render function - show question in div not alert&prompt message
 
 console.log(problems);
 console.log(score);
