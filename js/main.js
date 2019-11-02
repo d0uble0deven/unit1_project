6e58c7286b4bb3a99a94730eee64ef74fb89fa78
Dev Govindji <friendsofdev94@gmail.com>
2019-11-01 19:13:25 -0500

    breaks down data structure to include answer choices as a seperate key value pair to be rendered in a different html tag

--- js/main.js
+++ js/main.js
@@ -1,75 +1,93 @@
 //variables
 var problems = [
     {
-        question: "5 + 5 = ?" + "<br/>" + "<br/>" + "a) 10 &nbsp;&nbsp; b) 3 &nbsp;&nbsp; c) 16",
+        question: "5 + 5 = ?" + "<br/>" + "<br/>",
+        ac: "a) 10 &nbsp;&nbsp; b) 3 &nbsp;&nbsp; c) 16",
         answer: "a"
     },
     {
-        question: "2 + 2 = ?" + "<br/>" + "<br/>" + "a) 8 &nbsp;&nbsp; b) 4 &nbsp;&nbsp; c) 1",
+        question: "2 + 2 = ?" + "<br/>" + "<br/>",
+        ac: "a) 8 &nbsp;&nbsp; b) 4 &nbsp;&nbsp; c) 1",
         answer: "b"
     },
     {
-        question: "8 x 9 = ?" + "<br/>" + "<br/>" + "a) 86 &nbsp;&nbsp; b) 40 &nbsp;&nbsp; c) 72",
+        question: "8 x 9 = ?" + "<br/>" + "<br/>",
+        ac: "a) 86 &nbsp;&nbsp; b) 40 &nbsp;&nbsp; c) 72",
         answer: "c"
     },
     {
-        question: "6 - 2 = ?" + "<br/>" + "<br/>" + "a) 6 &nbsp;&nbsp; b) 40 &nbsp;&nbsp; c) 4",
+        question: "6 - 2 = ?" + "<br/>" + "<br/>",
+        ac: "a) 6 &nbsp;&nbsp; b) 40 &nbsp;&nbsp; c) 4",
         answer: "c"
     },
     {
-        question: "9 / 3 = ?" + "<br/>" + "<br/>" + "a) 3 &nbsp;&nbsp; b) 40 &nbsp;&nbsp; c) 4",
+        question: "9 / 3 = ?" + "<br/>" + "<br/>",
+        ac: "a) 3 &nbsp;&nbsp; b) 40 &nbsp;&nbsp; c) 4",
         answer: "a"
     },
     {
-        question: "7 x 8 = ?" + "<br/>" + "<br/>" + "a) 56 &nbsp;&nbsp; b) 42 &nbsp;&nbsp; c) 1",
+        question: "7 x 8 = ?" + "<br/>" + "<br/>",
+        ac: "a) 56 &nbsp;&nbsp; b) 42 &nbsp;&nbsp; c) 1",
         answer: "a"
     },
     {
-        question: "4 + 16 = ?" + "<br/>" + "<br/>" + "a) 6 &nbsp;&nbsp; b) 20 &nbsp;&nbsp; c) 4",
+        question: "4 + 16 = ?" + "<br/>" + "<br/>",
+        ac: "a) 6 &nbsp;&nbsp; b) 20 &nbsp;&nbsp; c) 4",
         answer: "b"
     },
     {
-        question: "19 - 7 = ?" + "<br/>" + "<br/>" + "a) 62 &nbsp;&nbsp; b) 9 &nbsp;&nbsp; c) 12",
+        question: "19 - 7 = ?" + "<br/>" + "<br/>",
+        ac: "a) 62 &nbsp;&nbsp; b) 9 &nbsp;&nbsp; c) 12",
         answer: "c"
     },
     {
-        question: "60 - 2 = ?" + "<br/>" + "<br/>" + "a) 58 &nbsp;&nbsp; b) 2 &nbsp;&nbsp; c) 14",
+        question: "60 - 2 = ?" + "<br/>" + "<br/>",
+        ac: "a) 58 &nbsp;&nbsp; b) 2 &nbsp;&nbsp; c) 14",
         answer: "a"
     },
     {
-        question: "50 + 50 = ?" + "<br/>" + "<br/>" + "a) 100 &nbsp;&nbsp; b) 13 &nbsp;&nbsp; c) 76",
+        question: "50 + 50 = ?" + "<br/>" + "<br/>",
+        ac: "a) 100 &nbsp;&nbsp; b) 13 &nbsp;&nbsp; c) 76",
         answer: "a"
     },
     {
-        question: "29 + 23 = ?" + "<br/>" + "<br/>" + "a) 82 &nbsp;&nbsp; b) 52 &nbsp;&nbsp; c) 59",
+        question: "29 + 23 = ?" + "<br/>" + "<br/>",
+        ac: "a) 82 &nbsp;&nbsp; b) 52 &nbsp;&nbsp; c) 59",
         answer: "b"
     },
     {
-        question: "1 x 7 = ?" + "<br/>" + "<br/>" + "a) 7 &nbsp;&nbsp; b) 17 &nbsp;&nbsp; c) 71",
+        question: "1 x 7 = ?" + "<br/>" + "<br/>",
+        ac: "a) 7 &nbsp;&nbsp; b) 17 &nbsp;&nbsp; c) 71",
         answer: "a"
     },
     {
-        question: "2 - 2 = ?" + "<br/>" + "<br/>" + "a) 80 &nbsp;&nbsp; b) 2 &nbsp;&nbsp; c) 0",
+        question: "2 - 2 = ?" + "<br/>" + "<br/>",
+        ac: "a) 80 &nbsp;&nbsp; b) 2 &nbsp;&nbsp; c) 0",
         answer: "c"
     },
     {
-        question: "12 / 6 = ?" + "<br/>" + "<br/>" + "a) 2 &nbsp;&nbsp; b) 7 &nbsp;&nbsp; c) 11",
+        question: "12 / 6 = ?" + "<br/>" + "<br/>",
+        ac: "a) 2 &nbsp;&nbsp; b) 7 &nbsp;&nbsp; c) 11",
         answer: "a"
     },
     {
-        question: "4 x 4 = ?" + "<br/>" + "<br/>" + "a) 5 &nbsp;&nbsp; b) 2 &nbsp;&nbsp; c) 16",
+        question: "4 x 4 = ?" + "<br/>" + "<br/>",
+        ac: "a) 5 &nbsp;&nbsp; b) 2 &nbsp;&nbsp; c) 16",
         answer: "c"
     },
     {
-        question: "41 + 16 = ?" + "<br/>" + "<br/>" + "a) 57 &nbsp;&nbsp; b) 28 &nbsp;&nbsp; c) 41",
+        question: "41 + 16 = ?" + "<br/>" + "<br/>",
+        ac: "a) 57 &nbsp;&nbsp; b) 28 &nbsp;&nbsp; c) 41",
         answer: "a"
     },
     {
-        question: "59 - 17 = ?" + "<br/>" + "<br/>" + "a) 42 &nbsp;&nbsp; b) 91 &nbsp;&nbsp; c) 22",
+        question: "59 - 17 = ?" + "<br/>" + "<br/>",
+        ac: "a) 42 &nbsp;&nbsp; b) 91 &nbsp;&nbsp; c) 22",
         answer: "a"
     },
     {
-        question: "6 - 5 = ?" + "<br/>" + "<br/>" + "a) 8 &nbsp;&nbsp; b) 23 &nbsp;&nbsp; c) 1",
+        question: "6 - 5 = ?" + "<br/>" + "<br/>",
+        ac: "a) 8 &nbsp;&nbsp; b) 23 &nbsp;&nbsp; c) 1",
         answer: "c"
     }
 ]
@@ -82,6 +100,7 @@ const sound = new Audio("audio/yay.wav");
 const mariachi = new Audio("audio/mariachi.wav");
 
 // cached elements
+var ac = document.getElementById('ac');
 var quest = document.getElementById('quest');
 var sco = document.getElementById('sco');
 var bttn1 = document.getElementById('bttn1');
@@ -131,6 +150,9 @@ function getRandomProb() {
     rndProb = Math.floor(Math.random() * problems.length);
     // stores randomly selected as prob
     prob = problems[rndProb].question;
+    ac = problems[rndProb].ac;
+    console.log(ac + "getRandomProb")
+
     // stores corresponding answer in to ans
     ans = problems[rndProb].answer;
     return prob;
@@ -163,6 +185,8 @@ function userWins() {
 function userCorrect() {
     // updates questions, answer, and score
     quest.innerHTML = prob;
+    ac.innerHTML = ac;
+    console.log(ac + "userCorrect")
     sco.innerHTML = score;
 
     // adds buttons
